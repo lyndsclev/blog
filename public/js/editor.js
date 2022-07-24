dc2a16f147b19af177f3e4a2740f673203c24c44
lyndsclev <lyndsclev@gmail.com>
2022-07-24 13:49:45 -0400

    set up firebase

--- public/js/editor.js
+++ public/js/editor.js
@@ -49,3 +49,37 @@ const addImage = (imagepath, alt) => {
     let textToInsert = `\r![${alt}](${imagepath})\r`; 
     articleField.value = articleField.value.slice(0, curPos) + textToInsert + articleField.value.slice(curPos);
 };
+
+// make publish button functional
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+publishBtn.addEventListener('click', () => {
+    if(articleField.value.length && blogTitleField.value.length) {
+        
+        // generate id
+        let letters = 'abcdefghijklmnopqrstuvwxyz'; 
+        let blogTitle = blogTitleField.value.split(" ").join("-"); 
+        let id = '';
+        for(let i = 0; i < 4; i++) {
+            id += letters[Math.floor(Math.random() * letters.length)]; 
+        }
+
+        // setting up docName 
+        let docName = `${blogTitle} - ${id}`; 
+        let date = new Date(); // for published at info
+
+        // access firestore with db variable 
+        db.collection("blogs").doc(docName).set({
+            title: blogTitleField.value,
+            article: articleField.value, 
+            bannerImage: bannerPath, 
+            publishedAt: `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+        })
+        .then(() => {
+            location.href = `/${docName}`; 
+        })
+        .catch((err) => {
+            console.error(err); 
+        })
+    }
+}); 
---
+++ public/js/firebase.js
@@ -0,0 +1,10 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyAMd5t5rGn96jOpzONkG8MY2vcME5ko1k0",
+    authDomain: "blog-website-b3a8e.firebaseapp.com",
+    projectId: "blog-website-b3a8e",
+    storageBucket: "blog-website-b3a8e.appspot.com",
+    messagingSenderId: "322594588664",
+    appId: "1:322594588664:web:b07f18908bfe2130e1380d"
+  };
+
+firebase.initializeApp(firebaseConfig);
